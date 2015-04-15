394445a6e2e720753779d5b79394785e6ec10779
Amos <wky1991@gmail.com>
2015-04-15 21:46:17 +0800

    refactor

--- parkingLot/parking_boy.rb
+++ parkingLot/parking_boy.rb
@@ -3,14 +3,12 @@ class ParkingBoy
     @parking_lots = parking_lots
   end
   def store car
-    @parking_lots.first.store(car)
+    parking_lot = @parking_lots.find {|parking_lot| parking_lot.space_left > 0 }
+    parking_lot.store car
   end
 
   def pick ticket
-    @parking_lots.each do |parking_lot|
-      if parking_lot.can_pick ticket
-        return parking_lot.pick ticket
-      end
-    end
+    parking_lot = @parking_lots.find {|parking_lot| parking_lot.can_pick ticket}
+    parking_lot.pick ticket if parking_lot != nil
   end
 end
